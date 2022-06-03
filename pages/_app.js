855419d8edd6633b3021615b981fa25395fb0640
Dilshan Ramesh <dilshanramesh81@gmail.com>
2022-06-03 12:53:27 +0530

    build issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <script src="https://cdn.directpay.lk/dev/v1/directpayCardPayment.js?v=1"></script>
+      <Script src="https://cdn.directpay.lk/dev/v1/directpayCardPayment.js?v=1"></Script>
       <Component {...pageProps} />
     </>
   )
